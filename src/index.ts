3d4feded5602b177614faf37ce057654e87b6be6
yi5 <github@yi5.dev>
2023-02-26 01:35:54 +0100

    Init

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import { Collection, Events, GatewayIntentBits, REST, Routes } from 'discord.js';
+// Importing clientClass
+import ClientClass from './structs/clientClass';
+
+///
+/// Creating the client
+///
+const client = new ClientClass({ // create a new discord client
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+  ],
+});
+
+export { client };
