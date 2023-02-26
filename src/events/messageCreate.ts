1a41a8cfe13d8561b0ab5ee979d1da76b1867905
yi5 <github@yi5.dev>
2023-02-26 01:45:43 +0100

    fixed imports

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,11 +1,10 @@
 // import message struct from discord.js
 import { Message } from 'discord.js';
-import { handlePhishingMessage } from '../utils/handlePhishing';
 
 export default async function (message: Message) {
   // Ignore all bots
   if (message.author.bot) return;
 
   // If the message is a phishing message, handle it
-  handlePhishingMessage(message);
+  console.log(`${message.author.username}#${message.author.discriminator} ${message.content}`);
 }
